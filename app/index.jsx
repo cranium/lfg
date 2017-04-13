20848e9c27adba4dcac2e432829fb5a0ca8f511c
Carson Crane <cranium@users.noreply.github.com>
2017-04-12 22:53:30 -0400

    remove bundle, update gitignore, update app index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,45 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-
-class LFG extends React.Component {
-    render () {
-        return (
-            <div className="lfg--block lfg--margin-bottom-4">
-                <div className="lfg--flex-row lfg--flex-between">
-                    <div className="lfg--position-relative">
-                        <img className="lfg--display-block-" src={ this.props.avatar_url } width="86" height="86"/>
-                        <span className="lfg--image-caption lfg--color-success">+1</span>
-                    </div>
-                    <div className="lfg--flex-grow-1 lfg--flex-basis-0 lfg--flex-column lfg--flex-around" style={{"margin-left": "8px"}}>
-                        <div className="lfg--text-uppercase lfg--text-bold">{ this.props.name }</div>
-                        <div className="lfg--color-secondary">
-                            <span className="lfg--tag"><i className="material-icons lfg--tag-icon">mic</i> mic required</span>
-                        </div>
-                        <div className="lfg--flex-row">
-                            <div className="lfg--text-center lfg--margin-0 lfg--progress-bar-outer lfg--position-relative lfg--flex-grow-1">
-                                <div className="lfg--progress-bar-inner" style={{width: 20 + '%' }}></div>
-                                <div className="lfg--padding-1"><span className="lfg--progress-bar-label">1/10</span></div>
-                            </div>
-                            <a className="lfg--margin-0 lfg--button lfg--margin-left-2" href={"/group/" + this.props.id }>Join</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-class MyClass extends React.Component {
-    render () {
-        var rows = this.props.lfgs.map((lfg) =>
-            <LFG key={lfg.id} {...lfg} />
-        );
-
-        return (<div>{rows}</div>);
-    }
-}
+import React from 'react';
+import LFGList from './components/LFGList.jsx';
 
 ReactDOM.render(
-    <MyClass lfgs={LFGS}/>,
+    <LFGList lfgs={LFGS}/>,
     document.getElementById('content')
 );
